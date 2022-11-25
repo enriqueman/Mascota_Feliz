e64f1d0ed9f98a498aa6042246ea086276527a0c
enriqueman <manzano.velasco217@gmail.com>
2022-11-24 22:22:53 -0500

    update

--- BackendMascotas/src/controllers/usuario.controller.ts
+++ BackendMascotas/src/controllers/usuario.controller.ts
@@ -1,3 +1,4 @@
+import { service } from '@loopback/core';
 import {
   Count,
   CountSchema,
@@ -19,11 +20,14 @@ import {
 } from '@loopback/rest';
 import {Usuario} from '../models';
 import {UsuarioRepository} from '../repositories';
-
+import { AutenticacionService } from '../services';
+const fetch =require('node-fetch');
 export class UsuarioController {
   constructor(
     @repository(UsuarioRepository)
     public usuarioRepository : UsuarioRepository,
+    @service(AutenticacionService)
+    public servicioAutenticacion : AutenticacionService
   ) {}
 
   @post('/usuarios')
@@ -44,7 +48,23 @@ export class UsuarioController {
     })
     usuario: Omit<Usuario, 'id'>,
   ): Promise<Usuario> {
-    return this.usuarioRepository.create(usuario);
+    let clave= this.servicioAutenticacion.GenerarClave();
+    let claveCifrada= this.servicioAutenticacion.CifrarClave(clave);
+    usuario.contrasena= claveCifrada
+    let p= await this.usuarioRepository.create(usuario);
+
+    //Notificar al nuevo usuario del sistema
+    let destino= usuario.correo;
+    let asunto = 'Bienvenido & Credenciales de acceso';
+    let contenido =`Hola ${usuario.nombre}, su usuario es ${usuario.correo} y su contraseña es ${clave}`;
+    
+    fetch(`https://127.0.0.1:5000/email?correo_destino=${destino}&asunto=${asunto}&contenido=${contenido}`)
+    .then((data: any) => {
+      console.log(data);
+    })
+
+    return p;
+  
   }
 
   @get('/usuarios/count')
