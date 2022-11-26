52815abd65c936c4135e2ea79157aa5953bd5492
enriqueman <manzano.velasco217@gmail.com>
2022-11-26 17:57:47 -0500

    componente seguridad modulo identificacion

--- FrondEndMascotas/FrontendMascotas/src/app/modulos/seguridad/seguridad.module.ts
+++ FrondEndMascotas/FrontendMascotas/src/app/modulos/seguridad/seguridad.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SeguridadRoutingModule } from './seguridad-routing.module';
+import { IdentificacionComponent } from './identificacion/identificacion.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    IdentificacionComponent
+  ],
   imports: [
     CommonModule,
     SeguridadRoutingModule
