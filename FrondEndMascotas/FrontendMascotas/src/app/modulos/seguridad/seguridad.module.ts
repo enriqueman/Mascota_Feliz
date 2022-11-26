2acad8b1fa607f69bc37a442e5811cc38074e8fd
enriqueman <manzano.velasco217@gmail.com>
2022-11-26 17:59:33 -0500

    componente cambiar clave

--- FrondEndMascotas/FrontendMascotas/src/app/modulos/seguridad/seguridad.module.ts
+++ FrondEndMascotas/FrontendMascotas/src/app/modulos/seguridad/seguridad.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { SeguridadRoutingModule } from './seguridad-routing.module';
 import { IdentificacionComponent } from './identificacion/identificacion.component';
+import { CambiarClaveComponent } from './cambiar-clave/cambiar-clave.component';
 
 
 @NgModule({
   declarations: [
-    IdentificacionComponent
+    IdentificacionComponent,
+    CambiarClaveComponent
   ],
   imports: [
     CommonModule,
