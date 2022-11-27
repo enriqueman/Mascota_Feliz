1348075e71501d1af8f9f93adc77aa38d0cc9fd0
enriqueman <manzano.velasco217@gmail.com>
2022-11-26 19:32:51 -0500

    añadir rutas a los modulos

--- FrondEndMascotas/FrontendMascotas/src/app/app-routing.module.ts
+++ FrondEndMascotas/FrontendMascotas/src/app/app-routing.module.ts
@@ -1,7 +1,41 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './plantilla/error/error.component';
+import { InicioComponent } from './plantilla/inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"inicio",
+    component: InicioComponent
+  },
+  {
+    path: "",
+    pathMatch:"full",
+    redirectTo:" inicio/"
+
+  },
+  {
+    path: "seguridad",
+    loadChildren: ()=> import("./modulos/seguridad/seguridad.module") .then(x=>x.SeguridadModule)
+   },
+
+   {
+    path: "administracion",
+    loadChildren: ()=> import("./modulos/administracion/administracion.module").then(x=>x.AdministracionModule)
+  },
+  { 
+    path:"servicios",
+    loadChildren: ()=> import("./modulos/servicios/servicios.module").then(x=>x.ServiciosModule)
+
+  },
+  {
+    path:"**",
+    component: ErrorComponent
+  }
+
+  
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
