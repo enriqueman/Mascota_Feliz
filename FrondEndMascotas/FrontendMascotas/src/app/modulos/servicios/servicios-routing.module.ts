7ae20cccf358287ec4b8d9f7b1a1e3dcad49ed5b
enriqueman <manzano.velasco217@gmail.com>
2022-11-26 20:09:17 -0500

    Rutas de servicios

--- FrondEndMascotas/FrontendMascotas/src/app/modulos/servicios/servicios-routing.module.ts
+++ FrondEndMascotas/FrontendMascotas/src/app/modulos/servicios/servicios-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdquirirProductosServiciosComponent } from './adquirir-productos-servicios/adquirir-productos-servicios.component';
+import { SolicitarAfiliacionComponent } from './solicitar-afiliacion/solicitar-afiliacion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"adquirir-productos-servicios",
+    component:AdquirirProductosServiciosComponent
+
+  },
+  {
+    path:"solicitar-afiliacion",
+    component:SolicitarAfiliacionComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
