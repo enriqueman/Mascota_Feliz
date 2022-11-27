9d08b036deafb05e0006d9b072830743a8ce0dd8
enriqueman <manzano.velasco217@gmail.com>
2022-11-26 20:09:00 -0500

    rutas de servicios

--- FrondEndMascotas/FrontendMascotas/src/app/modulos/seguridad/seguridad-routing.module.ts
+++ FrondEndMascotas/FrontendMascotas/src/app/modulos/seguridad/seguridad-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CambiarClaveComponent } from './cambiar-clave/cambiar-clave.component';
+import { IdentificacionComponent } from './identificacion/identificacion.component';
+import { RecuperarClaveComponent } from './recuperar-clave/recuperar-clave.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"cambiar-clave",
+    component:CambiarClaveComponent
+  },
+  {
+    path:"identificacion",
+    component:IdentificacionComponent
+  },
+  {
+    path:"recuperar-clave",
+    component:RecuperarClaveComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
